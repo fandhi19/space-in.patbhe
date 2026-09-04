8c59bae3d62ad01851666acc871866df6f22015c
fandhi19 <ilyasfandhianggara@gmail.com>
2026-09-04 23:30:55 +0700

    Update32

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit3d2d0bbc26ece2a174bff99ad59d3059
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3d2d0bbc26ece2a174bff99ad59d3059', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3d2d0bbc26ece2a174bff99ad59d3059', 'loadClassLoader'));
