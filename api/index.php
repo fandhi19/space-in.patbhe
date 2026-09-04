413cccdbb637f37b8ffdaeab31547c72a7342926
fandhi19 <ilyasfandhianggara@gmail.com>
2026-09-04 22:32:23 +0700

    Update1.3

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 
-// Forward ke index Laravel
+// Fix read-only filesystem di Vercel dengan mengarahkan compiled path ke /tmp
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+// Load Autoload & Framework Laravel
 require __DIR__ . '/../public/index.php';
